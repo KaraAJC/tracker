cbfa6a775a36fd55a5b40225def0398e8d199fc9
karaAJC <KaraAJC@gmail.com>
2016-09-01 14:19:52 -0500

    updates shoulda matcher config and passing user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "#Associations" do
-    it { should have_many(:trips).dependent(:destroy) }
-  end
 
-  describe "#Validations" do
-  end
+  it { should have_many(:trips).dependent(:destroy) }
+
 end
