de5f9ea8d80f7f2eef3cdcd2bbb70a4e4aeee096
karaAJC <KaraAJC@gmail.com>
2016-09-01 14:44:06 -0500

    updating user spec for omniauth test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:trips).dependent(:destroy) }
 
+  describe "#self.omniauth" do
+    let(:person) { User.Create }
+    it "checks the DB for a user by its' credentials" do
+      xexpect(:person).to matcher
+    end
+
+    it "creates a user with social auth credentials" do
+
+    end
+
+    it "does not create a new user if it finds an existing user" do
+
+    end
+
+  end
 end
